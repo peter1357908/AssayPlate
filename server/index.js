f1b7446db8a9677adb5d076c4716b60056727927
Peter Gao <peter1357908@hotmail.com>
2024-01-31 22:48:13 -0600

    re-enable CORS

--- server/index.js
+++ server/index.js
@@ -17,18 +17,13 @@ app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
 
-// app.use(
-//   cors({
-//     origin: [FRONTEND_URL],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
-
 app.use(
-  cors()
+  cors({
+    origin: [FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
 );
-
 
 app.use(cookieParser());
 app.use(express.json());
